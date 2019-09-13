0e77ab165d5b57add55c3e931019eac95121d733
mamrezn7 <mrz_shirazi@yahoo.com>
2019-09-14 01:52:21 +0430

    UI updated

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -15,9 +15,9 @@ export const Home = (props: IHomeProp) => {
 
   return (
     <Row>
-      <Col md="9">
+      <Col md={{ size: 6, offset: 3 }}>
         <h2>
-          <Translate contentKey="home.title">Welcome, Java Hipster!</Translate>
+          <span>Welcome to my Project!</span>
         </h2>
         <p className="lead">
           <Translate contentKey="home.subtitle">This is your homepage</Translate>
@@ -52,45 +52,46 @@ export const Home = (props: IHomeProp) => {
             </Alert>
           </div>
         )}
-        <p>
-          <Translate contentKey="home.question">If you have any question on JHipster:</Translate>
-        </p>
+        <span>Created by Mohammad reza shirazi</span>
+        {/*<p>*/}
+        {/*<Translate contentKey="home.question">If you have any question on JHipster:</Translate>*/}
+        {/*</p>*/}
 
-        <ul>
-          <li>
-            <a href="https://www.jhipster.tech/" target="_blank" rel="noopener noreferrer">
-              <Translate contentKey="home.link.homepage">JHipster homepage</Translate>
-            </a>
-          </li>
-          <li>
-            <a href="http://stackoverflow.com/tags/jhipster/info" target="_blank" rel="noopener noreferrer">
-              <Translate contentKey="home.link.stackoverflow">JHipster on Stack Overflow</Translate>
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/jhipster/generator-jhipster/issues?state=open" target="_blank" rel="noopener noreferrer">
-              <Translate contentKey="home.link.bugtracker">JHipster bug tracker</Translate>
-            </a>
-          </li>
-          <li>
-            <a href="https://gitter.im/jhipster/generator-jhipster" target="_blank" rel="noopener noreferrer">
-              <Translate contentKey="home.link.chat">JHipster public chat room</Translate>
-            </a>
-          </li>
-          <li>
-            <a href="https://twitter.com/java_hipster" target="_blank" rel="noopener noreferrer">
-              <Translate contentKey="home.link.follow">follow @java_hipster on Twitter</Translate>
-            </a>
-          </li>
-        </ul>
+        {/*<ul>*/}
+        {/*<li>*/}
+        {/*<a href="https://www.jhipster.tech/" target="_blank" rel="noopener noreferrer">*/}
+        {/*<Translate contentKey="home.link.homepage">JHipster homepage</Translate>*/}
+        {/*</a>*/}
+        {/*</li>*/}
+        {/*<li>*/}
+        {/*<a href="http://stackoverflow.com/tags/jhipster/info" target="_blank" rel="noopener noreferrer">*/}
+        {/*<Translate contentKey="home.link.stackoverflow">JHipster on Stack Overflow</Translate>*/}
+        {/*</a>*/}
+        {/*</li>*/}
+        {/*<li>*/}
+        {/*<a href="https://github.com/jhipster/generator-jhipster/issues?state=open" target="_blank" rel="noopener noreferrer">*/}
+        {/*<Translate contentKey="home.link.bugtracker">JHipster bug tracker</Translate>*/}
+        {/*</a>*/}
+        {/*</li>*/}
+        {/*<li>*/}
+        {/*<a href="https://gitter.im/jhipster/generator-jhipster" target="_blank" rel="noopener noreferrer">*/}
+        {/*<Translate contentKey="home.link.chat">JHipster public chat room</Translate>*/}
+        {/*</a>*/}
+        {/*</li>*/}
+        {/*<li>*/}
+        {/*<a href="https://twitter.com/java_hipster" target="_blank" rel="noopener noreferrer">*/}
+        {/*<Translate contentKey="home.link.follow">follow @java_hipster on Twitter</Translate>*/}
+        {/*</a>*/}
+        {/*</li>*/}
+        {/*</ul>*/}
 
-        <p>
-          <Translate contentKey="home.like">If you like JHipster, do not forget to give us a star on</Translate>{' '}
-          <a href="https://github.com/jhipster/generator-jhipster" target="_blank" rel="noopener noreferrer">
-            Github
-          </a>
-          !
-        </p>
+        {/*<p>*/}
+        {/*<Translate contentKey="home.like">If you like JHipster, do not forget to give us a star on</Translate>{' '}*/}
+        {/*<a href="https://github.com/jhipster/generator-jhipster" target="_blank" rel="noopener noreferrer">*/}
+        {/*Github*/}
+        {/*</a>*/}
+        {/*!*/}
+        {/*</p>*/}
       </Col>
       <Col md="3" className="pad">
         <span className="hipster rounded" />
